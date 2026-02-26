82e5b46db408038b0769116fa7397998c045bab5
Harsh <your-soniverma2006@gmail.com>
2026-02-27 01:37:03 +0530

    Initial Django commit for Vercel

--- home/views.py
+++ home/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
-
 def home_view(request):
-    return HttpResponse("<h1>Welcome to my live Django site!</h1>")
+    return render(request, 'index.html')
